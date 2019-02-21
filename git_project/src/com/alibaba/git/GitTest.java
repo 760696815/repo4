595f884e348ad01bb1e90b731648675bdfb43b67
hout <760696815@qq.com>
2019-02-21 20:06:29 +0800

    把本地工程添加到git

---
+++ git_project/src/com/alibaba/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.alibaba.git;
+
+/**
+ * @_
+ * @Author: hout
+ * @Date: created at 2019/2/21 19:59
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+        System.out.println("hello,houge");
+        System.out.println("hello,bajie");
+    }
+}
